73de8a32032fd43ee784df0b0d4d4df3c07e230d
ИСП-34 <-34@LAB17-10>
2023-03-27 12:11:47 +0300

    Добавьте файлы проекта.

---
+++ PiTPT1.2Kosov/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PiTPT1._2Kosov
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Height += 25;
+        }
+
+        private void Solving(object sender, RoutedEventArgs e)
+        {
+            if (!Int32.TryParse(Nletter.Text, out int N))
+            {
+                MessageBox.Show("Некорректные данные", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                int[] mass = new int[N];
+                Random rnd = new Random();
+                for (int i = 0; i < N; i++)
+                {
+                    mass[i] = rnd.Next(-9, 10);
+                }
+
+                int kol = 0;
+                for (int i = 0; i < N - 1; i++)
+                {
+                    if (mass[i] < 0 && mass[i + 1] >= 0) kol++;
+                    else if (mass[i] >= 0 && mass[i + 1] < 0) kol++;
+                }
+
+                string AllTime = "";
+                foreach (var nameee in mass)
+                {
+                    AllTime += nameee + "\n";
+                }
+
+                MessageBox.Show("Массив:\n" + AllTime + "\n" + "Кол-во смен знака: " + kol);
+            }
+        }
+    }
+}
